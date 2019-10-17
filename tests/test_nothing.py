103a24f193ee0e76520c4fc6ff123def5a4a7e7c
Etienne Thompson <etiennethompson@email.arizona.edu>
2019-10-17 11:55:16 -0700

    Adding blank tests.

---
+++ tests/test_nothing.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class TestNothing(unittest.TestCase):
+    def test_nothing_1(self):
+        pass
+
+    def test_nothing_2(self):
+        pass
+
+    def test_nothing_3(self):
+        pass
