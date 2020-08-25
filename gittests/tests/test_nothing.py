622ecbe1ce718ad30762f97d86bc94fff5201b54
Etienne Thompson <etiennethompson@on-campus-10-138-68-252.vpn.arizona.edu>
2020-08-25 09:42:53 -0700

    Setting up testing environment.

---
+++ gittests/tests/test_nothing.py
@@ -0,0 +1,13 @@
+"""Some blank tests for pre-push hook to run."""
+import unittest
+
+
+class TestNothing(unittest.TestCase):
+    def test_nothing_1(self):
+        print("hello world 2")
+
+    def test_nothing_2(self):
+        pass
+
+    def test_nothing_3(self):
+        pass
